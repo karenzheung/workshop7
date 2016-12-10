ac762490efc40943de657a58e6e8de76a55ee38c
Karen Zheung <karenzheung@gmail.com>
2016-12-09 19:57:20 -0500

    comments

--- server/src/server.js
+++ server/src/server.js
@@ -531,66 +531,113 @@ if (typeof(req.body) === 'string') {
     var fromUser = getUserIdFromToken(req.get('Authorization'));
     var comment = req.body;
     var author = req.body.author;
-    var feedItemId = req.params.feeditemid;
+    var feedItemId = new ObjectID(req.params.feeditemid);
     if (fromUser === author) {
-      var feedItem = readDocument('feedItems', feedItemId);
-      // Initialize likeCounter to empty.
       comment.likeCounter = [];
-      // Push returns the new length of the array.
-      // The index of the new element is the length of the array minus 1.
-      // Example: [].push(1) returns 1, but the index of the new element is 0.
-      var index = feedItem.comments.push(comment) - 1;
-      writeDocument('feedItems', feedItem);
-      // 201: Created.
-      res.status(201);
-      res.set('Location', '/feeditem/' + feedItemId + "/comments/" + index);
-      // Return a resolved version of the feed item.
-      res.send(getFeedItemSync(feedItemId));
-    } else {
-      // Unauthorized.
-      res.status(401).end();
-    }
+      db.collection('feedItems').updateOne({_id:feedItemId},
+        {
+          $push: {
+            comments:{
+              $each:[comment],
+              $position:0
+            }
+          }
+        },
+          function(err) {
+            if(err) {
+              sendDatabaseError(res, err);
+            } else {
+              res.status(201);
+              res.set('Location', '/feeditem/' + feedItemId + '/comments/' + 0);
+              getFeedItem(feedItemId, function(err, feedItem) {
+                if(err){
+                  sendDatabaseError(res, err);
+                } else {
+                  res.send(feedItem);
+                }
+              });
+            }
+          });
+        } else {
+          res.status(401).end();
+          console.log("Unauthorized");
+        }
   });
 
   app.put('/feeditem/:feeditemid/comments/:commentindex/likelist/:userid', function(req, res) {
     var fromUser = getUserIdFromToken(req.get('Authorization'));
-    var userId = parseInt(req.params.userid, 10);
-    var feedItemId = parseInt(req.params.feeditemid, 10);
+    var userId = req.params.userid;
+    var feedItemId = new ObjectID(req.params.feeditemid);
     var commentIdx = parseInt(req.params.commentindex, 10);
     // Only a user can mess with their own like.
     if (fromUser === userId) {
-      var feedItem = readDocument('feedItems', feedItemId);
-      var comment = feedItem.comments[commentIdx];
-      // Only change the likeCounter if the user isn't in it.
-      if (comment.likeCounter.indexOf(userId) === -1) {
-        comment.likeCounter.push(userId);
-      }
-      writeDocument('feedItems', feedItem);
-      comment.author = readDocument('users', comment.author);
-      // Send back the updated comment.
-      res.send(comment);
-    } else {
-      // Unauthorized.
-      res.status(401).end();
-    }
+      db.collection('feedItems').updateOne({_id:feedItemId},
+        {
+          $addToSet:{[`comments.${commentIdx}.likeCounter`]:new ObjectID(userId)}
+        },
+        function(err){
+          if(err) {
+            sendDatabaseError(res, err);
+          } else {
+            getFeedItem(feedItemId, function(err, feedItem){
+              if(err){
+                sendDatabaseError(res, err);
+              } else {
+                var comment = feedItem.comments[commentIdx];
+                db.collection('users').findOne({_id:comment.author._id},
+                function(err, user) {
+                  if(err) {
+                    sendDatabaseError(res, err);
+                  } else {
+                    comment.author = user;
+                    res.status(201);
+                    res.send(comment);
+                  }
+                });
+              }
+            });
+          }
+          });
+        } else {
+          res.status(401).end();
+        }
+
   });
 
   app.delete('/feeditem/:feeditemid/comments/:commentindex/likelist/:userid', function(req, res) {
     var fromUser = getUserIdFromToken(req.get('Authorization'));
-    var userId = parseInt(req.params.userid, 10);
-    var feedItemId = parseInt(req.params.feeditemid, 10);
+    var userId = req.params.userid;
+    var feedItemId = new ObjectID(req.params.feeditemid);
     var commentIdx = parseInt(req.params.commentindex, 10);
     // Only a user can mess with their own like.
     if (fromUser === userId) {
-      var feedItem = readDocument('feedItems', feedItemId);
-      var comment = feedItem.comments[commentIdx];
-      var userIndex = comment.likeCounter.indexOf(userId);
-      if (userIndex !== -1) {
-        comment.likeCounter.splice(userIndex, 1);
-        writeDocument('feedItems', feedItem);
-      }
-      comment.author = readDocument('users', comment.author);
-      res.send(comment);
+      db.collection('feedItems').updateOne({_id: feedItemId},
+      {
+        $pull: {[`comments.${commentIdx}.likeCounter`]:new ObjectID(userId)}
+        },
+        function(err) {
+          if(err) {
+            return sendDatabaseError(res, err);
+          } else {
+            getFeedItem(feedItemId, function(err, feedItem) {
+              if(err) {
+                return sendDatabaseError(res, err);
+              } else{
+                var comment = feedItem.comments[commentIdx];
+                db.collection('users').findOne({_id:comment.author._id},
+                function(err, user) {
+                  if(err) {
+                    return sendDatabaseError(res, err);
+                  } else {
+                    comment.author=user;
+                    res.status(201);
+                    res.send(comment);
+                  }
+                });
+              }
+            });
+          }
+        });
     } else {
       // Unauthorized.
       res.status(401).end();
@@ -604,7 +651,7 @@ if (typeof(req.body) === 'string') {
       res.send();
     });
   });
-  
+
   /**
    * Translate JSON Schema Validation failures into error 400s.
    */
